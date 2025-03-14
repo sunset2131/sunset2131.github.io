408843e204f58b97733817fb64162da203f81177
Sunset <gongli20040422@163.com>
2025-03-14 08:52:21 +0800

    domain-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sunset-blog.top/",
+  site: "https://www.sunset-blog.top/",
   // base: "/",
   trailingSlash: "always",
   integrations: [
